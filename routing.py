418edb7bb5b6ce6847dbde1e5e24f4c14a402ecf
Lucia Reynoso <lvreynoso@protonmail.com>
2019-05-11 17:51:14 -0700

    Add exception handling to loading and pricing routes

--- routing.py
+++ routing.py
@@ -5,44 +5,51 @@ from hashtable import RoutingTable
 import sys, itertools, resource, csv, pickle
 
 def loadRoutes(path, datastore):
-    spinner = itertools.cycle('-\\|/')
-    print('Loading call routing data...', end='')
-    count = 0
-    with open(path) as datafile:
-        reader = csv.reader(datafile, delimiter=',')
-        for row in reader:
-            sys.stdout.write(next(spinner))
-            sys.stdout.flush()
-            datastore.insert(row[0], row[1])
-            count += 1
-            sys.stdout.write('\b')
-    sys.stdout.write('Done.\n')
-    sys.stdout.flush()
-    print('{} routes processed'.format(count))
-    print('Routing table entries: {}'.format(datastore.size))
-    print('Memory usage: {} MiB'.format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss >> 20))
+    try:
+        spinner = itertools.cycle('-\\|/')
+        print('Loading call routing data...', end='')
+        count = 0
+        with open(path) as datafile:
+            reader = csv.reader(datafile, delimiter=',')
+            for row in reader:
+                sys.stdout.write(next(spinner))
+                sys.stdout.flush()
+                datastore.insert(row[0], row[1])
+                count += 1
+                sys.stdout.write('\b')
+        sys.stdout.write('Done.\n')
+        sys.stdout.flush()
+        print('{} routes processed'.format(count))
+        print('Routing table entries: {}'.format(datastore.size))
+        print('Memory usage: {} MiB'.format(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss >> 20))
+    except Exception as e:
+        print('Unable to load routes from {}: {}'.format(path, e))
+        return Error('loadRouteError')
 
 def priceRoutes(path, datastore):
-    spinner = itertools.cycle('-\\|/')
-    print('Pricing phone numbers...', end='')
-    count = 0
-    results = []
-    with open(path) as datafile:
-        reader = csv.reader(datafile, delimiter=',')
-        for row in reader:
-            sys.stdout.write(next(spinner))
-            sys.stdout.flush()
-            price = datastore.lookup(row[0])
-            results.append((row[0], price))
-            count += 1
-            sys.stdout.write('\b')
-    savePath = ''.join([path[:len(path) - 4], '-priced.txt'])
-    with open(savePath, 'w') as printfile:
-        writer = csv.writer(printfile)
-        writer.writerows(results)
-    sys.stdout.write('Done.\n')
-    sys.stdout.flush()
-    print('Wrote {} prices to {}'.format(count, savePath))
+    try:
+        spinner = itertools.cycle('-\\|/')
+        print('Pricing phone numbers...', end='')
+        count = 0
+        results = []
+        with open(path) as datafile:
+            reader = csv.reader(datafile, delimiter=',')
+            for row in reader:
+                sys.stdout.write(next(spinner))
+                sys.stdout.flush()
+                price = datastore.lookup(row[0])
+                results.append((row[0], price))
+                count += 1
+                sys.stdout.write('\b')
+        savePath = ''.join([path[:len(path) - 4], '-priced.txt'])
+        with open(savePath, 'w') as printfile:
+            writer = csv.writer(printfile)
+            writer.writerows(results)
+        sys.stdout.write('Done.\n')
+        sys.stdout.flush()
+        print('Wrote {} prices to {}'.format(count, savePath))
+    except Exception as e:
+        print('Unable to price routes to {}: {}'.format(path, e))
 
 def shell(data):
     # start a shell
